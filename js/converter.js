be5ac8d4612824a0dc7086772c292418c5e5caab
Alberto Gonzalez <al286290@uji.es>
2016-01-07 22:42:00 +0100

    Models & model transpiler added

---
+++ js/converter.js
@@ -0,0 +1,36 @@
+(function (fileName) {
+  var indices = null;
+  var vertices = null;
+
+  var rawFile = new XMLHttpRequest();
+  rawFile.overrideMimeType('application/json');
+  rawFile.open('GET', fileName, true);
+  rawFile.onload = function() {
+    var model = JSON.parse(rawFile.responseText);
+    indices = model.connectivity[0].indices;
+    vertices = [];
+    var verticesTri = [];
+    var normals = [];
+    var colors = [];
+
+    for (var i = 0; i < model.vertices[0].values.length; i += 3) {
+      verticesTri.push([model.vertices[0].values[i], model.vertices[0].values[i+1], model.vertices[0].values[i+2]]);
+    }
+
+    for (var i = 0; i < model.vertices[1].values.length; i+= 3) {
+      normals.push([model.vertices[1].values[i], model.vertices[1].values[i+1], model.vertices[1].values[i+2]]);
+    }
+
+    for (var i = 0; i < model.vertices[2].values.length; i+=4) {
+      colors.push([model.vertices[2].values[i], model.vertices[2].values[i+1], model.vertices[2].values[i+2]]);
+    }
+
+    for (var i = 0; i < verticesTri.length; i++) {
+      vertices.push(verticesTri[i][0], verticesTri[i][1], verticesTri[i][2]);
+      vertices.push(normals[i][0], normals[i][1], normals[i][2]);
+      vertices.push(colors[i][0], colors[i][1], colors[i][2]);
+    }
+    console.log('OK');
+  };
+  rawFile.send();
+})('models/BigPalmTree.json');
